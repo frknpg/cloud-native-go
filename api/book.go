a6b459d5b4577db8ae8d2494af57f6d5758e30e8
furkan <frknpg@gmail.com>
2020-04-22 01:48:24 +0300

    comments added

--- api/book.go
+++ api/book.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+//Book object
 type Book struct {
 	Title       string `json:"title"`
 	Author      string `json:"author"`
@@ -13,6 +14,7 @@ type Book struct {
 	Description string `json:"description,omitempty`
 }
 
+//ToJSON book to byte array
 func (b Book) ToJSON() []byte {
 	ToJSON, err := json.Marshal(b)
 	if err != nil {
@@ -21,6 +23,7 @@ func (b Book) ToJSON() []byte {
 	return ToJSON
 }
 
+//FromJSON res body to book struct
 func FromJSON(data []byte) Book {
 	book := Book{}
 	err := json.Unmarshal(data, &book)
@@ -30,11 +33,13 @@ func FromJSON(data []byte) Book {
 	return book
 }
 
+//Books sample data
 var Books = map[string]Book{
 	"078945613": Book{Title: "Book 1", Author: "Author 1", ISBN: "078945613"},
 	"054623897": Book{Title: "Book 2", Author: "Author 2", ISBN: "054623897"},
 }
 
+//BookHandeFunc  handler for book get, update and delete acriosn
 func BookHandeFunc(w http.ResponseWriter, r *http.Request) {
 	isbn := r.URL.Path[len("/api/books/"):]
 
@@ -70,6 +75,7 @@ func BookHandeFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//BooksHandeFunc  handler for get all books and post a book
 func BooksHandeFunc(w http.ResponseWriter, r *http.Request) {
 	switch method := r.Method; method {
 	case http.MethodGet:
@@ -94,6 +100,7 @@ func BooksHandeFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//AllBooks return all books
 func AllBooks() []Book {
 	values := make([]Book, len(Books))
 	idx := 0
@@ -104,6 +111,7 @@ func AllBooks() []Book {
 	return values
 }
 
+//UpdateBook book update
 func UpdateBook(isbn string, book Book) bool {
 	_, founded := Books[isbn]
 	if founded {
@@ -112,15 +120,18 @@ func UpdateBook(isbn string, book Book) bool {
 	return founded
 }
 
+//DeleteBook delete a book
 func DeleteBook(isbn string) {
 	delete(Books, isbn)
 }
 
+//GetBook get specified book
 func GetBook(isbn string) (Book, bool) {
 	book, founded := Books[isbn]
 	return book, founded
 }
 
+//CreateBook new book create
 func CreateBook(book Book) (string, bool) {
 	_, exist := Books[book.ISBN]
 	if exist {
